e9816f6b8f09fa176cf80c8c0793c47b28cd8789
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2022-11-27 20:15:31 +0400

    Add video poster

--- src/components/HomePageComponents/VideoExplanation/VideoExplanation.js
+++ src/components/HomePageComponents/VideoExplanation/VideoExplanation.js
@@ -1,19 +1,40 @@
 import React from 'react';
 import s from './VideoExplanation.module.scss';
+import poster from '../../../assets/poster.png';
 
 function VideoExplanation() {
+  const [status, setStatus] = React.useState({});
+
+  const renderPoster = () => {
+    if (status.isLoaded) {
+      return null;
+    }
+    return (
+      <div>
+        <img src={poster} alt="poster" className={s.videoPoster} />
+      </div>
+    );
+  };
+
   return (
     <section className={s.videoExplanationSection}>
       <h2>How are Nexos tokens Issued</h2>
-      <video width="100%" height="200" controls preload="metadata" playsInline>
-        <source
-          src={
-            'https://firebasestorage.googleapis.com/v0/b/nexos-879bb.appspot.com/o/promo.mp4?alt=media&token=f9babe1c-7451-42bd-acfd-06be7dbf4636'
-          }
-          type="video/mp4"
-        />
-        Your browser does not support the video tag.
-      </video>
+      <div className={s.videoContainer}>
+        <video
+          controls
+          preload="metadata"
+          playsInline
+          // poster={poster}
+        >
+          <source
+            src={
+              'https://firebasestorage.googleapis.com/v0/b/nexos-879bb.appspot.com/o/promo.mp4?alt=media&token=f9babe1c-7451-42bd-acfd-06be7dbf4636'
+            }
+            type="video/mp4"
+          />
+          Your browser does not support the video tag.
+        </video>
+      </div>
     </section>
   );
 }

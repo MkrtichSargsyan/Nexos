e54be08d21188b2ea3067e646ff58605efab793d
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2022-11-27 20:31:01 +0400

    Fix header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -19,14 +19,12 @@ function Header({ openSidebar }) {
       <div className="navItems">
         <NavLink to={'why-nexos'}>Why Nexos</NavLink>
         <NavLink to={'how-it-works'}>How it works</NavLink>
-        <NavLink>News</NavLink>
-        <NavLink>Nexos Gold</NavLink>
-        <NavLink>Transparency</NavLink>
+    
       </div>
-      <div className="registrationItems">
+      {/* <div className="registrationItems">
         <span className="loginBtn">Log In</span>
         <span className="signinBtn">Sign Up</span>
-      </div>
+      </div> */}
       <MenuContainer to={'/'}>
         <img src={menu} alt="menu" onClick={() => openSidebar(true)} />
       </MenuContainer>
